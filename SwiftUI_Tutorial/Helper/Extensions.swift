28c6d2f3bf05cdbc40422de0c3d4fea2c464ef70
Hau Nguyen <huuhau.hh47@gmail.com>
2022-09-15 14:48:10 +0700

    ExpandableText

--- SwiftUI_Tutorial/Helper/Extensions.swift
+++ SwiftUI_Tutorial/Helper/Extensions.swift
@@ -94,3 +94,5 @@ struct GeometryGetterMod: ViewModifier {
         }
     }
 }
+
+
--- SwiftUI_Tutorial/Helper/Utilities.swift
+++ SwiftUI_Tutorial/Helper/Utilities.swift
@@ -21,4 +21,23 @@ public class Utilities {
         UIDevice.current.setValue(orientation.rawValue, forKey: "orientation")
         UINavigationController.attemptRotationToDeviceOrientation()
     }
+    
+    // Example:
+//    textWithHashtags("Here's a quick and easy way to include #hashtag formatting in your #SwiftUI Text Views. Learn how in the #Tutorial here.", color: .red)
+//        .font(Font.custom("Avenir Next", size: 20))
+//        .bold()
+    public static func textWithHashtags(_ text: String, color: Color) -> Text {
+        let words = text.split(separator: " ")
+        var output: Text = Text("")
+
+        for word in words {
+            if word.hasPrefix("#") { // Pick out hash in words
+                output = output + Text(" ") + Text(String(word))
+                    .foregroundColor(color) // Add custom styling here
+            } else {
+                output = output + Text(" ") + Text(String(word))
+            }
+        }
+        return output
+    }
 }
